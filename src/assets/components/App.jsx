49fb99dd5938e72693f574d7f6bd6a2685cd7e90
NikaBotchorishvili <nikabotchorishvili@gmail.com>
2022-08-09 18:38:53 +0400

    get memes from an api

--- src/assets/components/App.jsx
+++ src/assets/components/App.jsx
@@ -3,10 +3,12 @@ import "../css/App.css"
 import Navbar from "./Navbar"
 import Form from "./Form"
 export default function App() {
+  
   return (
     <>
       <Navbar />
       <Form />
+
     </>
   )
 }
--- src/assets/components/Form.jsx
+++ src/assets/components/Form.jsx
@@ -1,21 +1,28 @@
-import React from "react";
-import data from "./data";
+import React, { useState, useEffect } from "react";
 export default function Form() {
-	const [meme, setMeme] = React.useState({
+	const [allMemes, setAllMemes] = useState({});
+
+	const [meme, setMeme] = useState({
 		topText: "",
 		bottomText: "",
-		image: "http://i.imgflip.com/1bij.jpg",
+		image: "",
 	});
-	const [allImages, setAllMemeImages] = React.useState(data.data.memes);
-	const [memeText, setMemeText] = React.useState({
+
+	useEffect(() => {
+		fetch("https://api.imgflip.com/get_memes")
+			.then(res => res.json())
+			.then(data => setAllMemes(data.data.memes));
+	}, []);
+
+	const [memeText, setMemeText] = useState({
 		topText: "",
 		bottomText: "",
 	});
 
 	function randomImage() {
-		let randomIndex = Math.floor(Math.random() * allImages.length);
+		let randomIndex = Math.floor(Math.random() * allMemes.length);
 
-		let randomImage = allImages[randomIndex].url;
+		let randomImage = allMemes[randomIndex].url;
 		setMeme((prevMeme) => {
 			return {
 				...prevMeme,
@@ -24,23 +31,34 @@ export default function Form() {
 		});
 	}
 
-    function handleChange(event){
-        const {name, value} = event.target;
-        setMemeText((prevMemeText) => {
-            return {
-                ...prevMemeText,
-                [name]: value,
-            }
-        })
-    }
+	function handleChange(event) {
+		const { name, value } = event.target;
+		setMemeText((prevMemeText) => {
+			return {
+				...prevMemeText,
+				[name]: value,
+			};
+		});
+	}
 	return (
 		<>
 			{" "}
 			<section className="meme-form">
 				<div className="meme-cont">
 					<div className="inputs">
-						<input onChange={ handleChange } value={ memeText.topText } name="topText" type="text" placeholder="Enter top text" />
-						<input onChange={ handleChange } value={ memeText.bottomText } name="bottomText" type="text" placeholder="Enter Bottom text"
+						<input
+							onChange={handleChange}
+							value={memeText.topText}
+							name="topText"
+							type="text"
+							placeholder="Enter top text"
+						/>
+						<input
+							onChange={handleChange}
+							value={memeText.bottomText}
+							name="bottomText"
+							type="text"
+							placeholder="Enter Bottom text"
 						/>
 					</div>
 					<button onClick={randomImage}>Generate a new meme image</button>
@@ -48,8 +66,8 @@ export default function Form() {
 			</section>
 			<div className="meme">
 				<img src={meme.image} className="meme--image" />
-				<h2 className="meme--text top">{ memeText.topText }</h2>
-				<h2 className="meme--text bottom">{ memeText.bottomText }</h2>
+				<h2 className="meme--text top">{memeText.topText}</h2>
+				<h2 className="meme--text bottom">{memeText.bottomText}</h2>
 			</div>
 		</>
 	);
